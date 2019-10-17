a7f3ad3fcf8ca0ef4b050d6f029f87933742741e
Cyrus <cyrus.tanade.16@ucl.ac.uk>
2019-10-17 11:44:32 +0100

    friend groups structure

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+        'name': {
+            'Jill': {
+                'age':{26},
+                'job':{'biologist'},
+                'relationship':{
+                    'friend': ['Zalika'],
+                    'partner': ['John']
+                    },
+                },
+            'Zalika': {
+                'age':{28},
+                'job':{'artist'},
+                'relationship':{
+                    'friend': ['Jill']
+                    },
+                },
+            'John':{
+                'age':{27},
+                'job':{'writer'},
+                'relationship':{
+                    'partner':['Jill']
+                    },
+                },
+            'Nash':{
+                'age':{34},
+                'job':{'chef'},
+                'relationship':{
+                    'cousin':['John'],
+                    'lanlord':['Zalika']
+                },
+            }
+        }
+    }
